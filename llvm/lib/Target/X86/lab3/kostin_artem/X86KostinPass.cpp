ccd82a6ab3a6556484eed1600f6305a81d06633b
aartyomm <artem-kostin2013.kostin@yandex.ru>
2024-04-24 22:06:02 +0300

    test5

--- llvm/lib/Target/X86/lab3/kostin_artem/X86KostinPass.cpp
+++ llvm/lib/Target/X86/lab3/kostin_artem/X86KostinPass.cpp
@@ -3,86 +3,82 @@
 #include "X86Subtarget.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/Register.h"
+#include <map>
 
 using namespace llvm;
-
-#define NAME "x86-kostin-pass"
-#define DESC "pass replacing mul and add with muladd"
-
-#define DEBUG_TYPE AVOIDCALL_NAME
 
 namespace {
 class KostinPass : public MachineFunctionPass {
 public:
   static char ID;
   KostinPass() : MachineFunctionPass(ID) {}
-  bool runOnMachineFunction(MachineFunction &MF) override;
 
-private:
-  StringRef getPassName() const override { return DESC; }
-};
-}
+  bool runOnMachineFunction(MachineFunction &MF) override {
+    bool Modified = false;
+    const TargetInstrInfo *TII = MF.getSubtarget().getInstrInfo();
 
-char KostinPass::ID = 0;
+    for (auto &MBB : MF) {
+      std::map<llvm::Register, std::pair<MachineInstr *, MachineInstr *>>
+          useMap;
+      for (auto I = MBB.begin(); I != MBB.end(); I++) {
+        if (I->getOpcode() == X86::MULPDrr) {
+          useMap.insert({I->getOperand(0).getReg(), {&(*I), nullptr}});
+        } else {
+          for (unsigned i = 1; i < I->getNumOperands(); i++) {
+            llvm::MachineOperand &MO = I->getOperand(i);
+            if (MO.isReg() && MO.getReg().isVirtual()) {
+              auto r = MO.getReg();
+              auto fnd = useMap.find(r);
+              if (fnd != useMap.end()) {
+                if (fnd->second.second) {
+                  useMap.erase(r);
+                } else {
+                  useMap.insert_or_assign(
+                      r, std::make_pair(fnd->second.first, &(*I)));
+                }
+              }
+            }
+          }
+        }
+      }
+      for (auto u : useMap) {
+        if (u.second.second && u.second.second->getOpcode() == X86::ADDPDrr) {
+          auto &MulInstr = *u.second.first; // a = b * c;
+          auto &AddInstr =
+              *u.second
+                   .second; // d = e(a) + f | d = e + f(a);  => d = b * c + e
 
-FunctionPass *llvm::createX86KostinPass() {
-  return new KostinPass();
-}
+          auto a = MulInstr.getOperand(0).getReg();
+          auto b = MulInstr.getOperand(1).getReg();
+          auto c = MulInstr.getOperand(2).getReg();
+          auto d = AddInstr.getOperand(0).getReg();
+          auto e = AddInstr.getOperand(1).getReg();
+          auto f = AddInstr.getOperand(2).getReg();
+          if (e == a) {
+            e = f;
+          }
 
-INITIALIZE_PASS(KostinPass, NAME, DESC, false, false)
-
-static bool isAddInstr(MachineInstr &MI) {
-  return MI.getOpcode() == X86::ADDPDrr || MI.getOpcode() == X86::ADDPDrm;
-}
-
-static bool isMulInstr(MachineInstr &MI) {
-  return MI.getOpcode() == X86::MULPDrr || MI.getOpcode() == X86::MULPDrm;
-}
-
-static bool existInvector(std::vector<MachineInstr *> *vector,
-                          MachineInstr *val) {
-  return std::find(vector->begin(), vector->end(), val) == vector->end();
-}
-
-bool KostinPass::runOnMachineFunction(MachineFunction &MF) {
-  const X86Subtarget &STI = MF.getSubtarget<X86Subtarget>();
-  const X86InstrInfo &TII = *STI.getInstrInfo();
-
-  std::vector<MachineInstr *> MIvector;
-
-  for (auto &MBB : MF) {
-    for (auto &MI : MBB) {
-      if (!(isMulInstr(MI)))
-        continue;
-
-      auto &op = MI.getOperand(0);
-      for (auto &secondMI : MBB) {
-        int ind = secondMI.findRegisterUseOperandIdx(op.getReg());
-        if (ind != -1 && isAddInstr(secondMI)) {
-          if (ind == 0)
-            break;
-          int second_ind = 3 - ind;
-
-          MIMetadata MIMD(MI);
-          MachineInstr *Mul = &MI;
-          MachineInstr *Add = &secondMI;
-
-          BuildMI(MBB, Mul, MIMD, TII.get(X86::VFMADD213PDr),
-                  Add->getOperand(0).getReg())
-              .addReg(Mul->getOperand(1).getReg())
-              .addReg(Mul->getOperand(2).getReg())
-              .addReg(Add->getOperand(second_ind).getReg());
-
-          if (existInvector(&MIvector, Mul))
-            MIvector.push_back(Mul);
-          if (existInvector(&MIvector, Add))
-            MIvector.push_back(Add);
+          MIMetadata MIMD(AddInstr);
+          BuildMI(MBB, AddInstr, MIMD,
+                  TII->get(X86::VFMADD213PDr)) // or X86::VFMADDPD4rr
+              .addReg(d, RegState::Define)
+              .addReg(b)
+              .addReg(c)
+              .addReg(e);
+          MulInstr.eraseFromParent();
+          AddInstr.eraseFromParent();
+          Modified = true;
         }
       }
     }
+
+    return Modified;
   }
-  for (auto &MI : MIvector) {
-    MI->eraseFromParent();
-  }
-  return true;
+};
+
 }
+
+char KostinPass::ID = 0;
+static RegisterPass<KostinPass> X("x86-kostin-pass", "X86 Kostin Pass",
+                                         false, false);
