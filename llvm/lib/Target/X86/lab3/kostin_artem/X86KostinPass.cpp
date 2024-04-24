6beaa18db42028706ff70ebe208857e4572b77a4
aartyomm <artem-kostin2013.kostin@yandex.ru>
2024-04-25 01:52:04 +0300

    fixes, clang format

--- llvm/lib/Target/X86/lab3/kostin_artem/X86KostinPass.cpp
+++ llvm/lib/Target/X86/lab3/kostin_artem/X86KostinPass.cpp
@@ -3,69 +3,73 @@
 #include "X86Subtarget.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
-#include <utility>
+#include "llvm/Pass.h"
 #include <vector>
 
 using namespace llvm;
 
 namespace {
+
 class X86KostinPass : public MachineFunctionPass {
 public:
   static char ID;
   X86KostinPass() : MachineFunctionPass(ID) {}
+
   bool runOnMachineFunction(MachineFunction &MF) override;
+
+private:
+  const TargetInstrInfo *TII;
+  std::vector<std::pair<MachineInstr *, MachineInstr *>> ToReplace;
+
+  void identifyReplaceableInstructions(MachineFunction &MF);
+  void replaceInstructions(MachineFunction &MF);
 };
 
 char X86KostinPass::ID = 0;
 
-bool X86KostinPass::runOnMachineFunction(MachineFunction &machineFunc) {
-  const TargetInstrInfo *instrInfo = machineFunc.getSubtarget().getInstrInfo();
-  bool changed = false;
+bool X86KostinPass::runOnMachineFunction(MachineFunction &MF) {
+  TII = MF.getSubtarget().getInstrInfo();
+  identifyReplaceableInstructions(MF);
+  if (ToReplace.empty())
+    return false;
+  replaceInstructions(MF);
+  return true;
+}
 
-  std::vector<std::pair<MachineInstr *, MachineInstr *>> deletedInstructions;
-
-  for (auto &block : machineFunc) {
-    MachineInstr *mulInstr = nullptr;
-    MachineInstr *addInstr = nullptr;
-
-    for (auto op = block.begin(); op != block.end(); ++op) {
-      if (op->getOpcode() == X86::MULPDrr) {
-        mulInstr = &(*op);
-
-        for (auto opNext = std::next(op); opNext != block.end(); ++opNext) {
-          if (opNext->getOpcode() == X86::ADDPDrr) {
-            addInstr = &(*opNext);
-
-            if (mulInstr->getOperand(0).getReg() ==
-                addInstr->getOperand(1).getReg()) {
-              deletedInstructions.emplace_back(mulInstr, addInstr);
-              changed = true;
-              break;
-            }
-          } else if (opNext->definesRegister(mulInstr->getOperand(0).getReg()))
-            break;
+void X86KostinPass::identifyReplaceableInstructions(MachineFunction &MF) {
+  for (auto &MBB : MF) {
+    MachineInstr *Mul = nullptr;
+    for (auto &MI : MBB) {
+      if (MI.getOpcode() == X86::MULPDrr) {
+        Mul = &MI;
+      } else if (MI.getOpcode() == X86::ADDPDrr && Mul) {
+        if (Mul->getOperand(0).getReg() == MI.getOperand(1).getReg()) {
+          ToReplace.emplace_back(Mul, &MI);
+          Mul = nullptr;
         }
+      } else if (MI.definesRegister(Mul ? Mul->getOperand(0).getReg() : 0)) {
+        Mul = nullptr;
       }
     }
   }
+}
 
-  for (auto &[mulInstr, addInstr] : deletedInstructions) {
-    MachineInstrBuilder MIB =
-        BuildMI(*mulInstr->getParent(), *mulInstr, mulInstr->getDebugLoc(),
-                instrInfo->get(X86::VFMADD213PDZ128r));
-
-    MIB.addReg(addInstr->getOperand(0).getReg(), RegState::Define);
-    MIB.addReg(mulInstr->getOperand(1).getReg());
-    MIB.addReg(mulInstr->getOperand(2).getReg());
-    MIB.addReg(addInstr->getOperand(2).getReg());
-
-    mulInstr->eraseFromParent();
-    addInstr->eraseFromParent();
+void X86KostinPass::replaceInstructions(MachineFunction &MF) {
+  for (auto &Pair : ToReplace) {
+    MachineInstr *Mul = Pair.first;
+    MachineInstr *Add = Pair.second;
+    BuildMI(*Mul->getParent(), *Mul, Mul->getDebugLoc(),
+            TII->get(X86::VFMADD213PDZ128r))
+        .addReg(Add->getOperand(0).getReg(), RegState::Define)
+        .addReg(Mul->getOperand(1).getReg())
+        .addReg(Mul->getOperand(2).getReg())
+        .addReg(Add->getOperand(2).getReg());
+    Mul->eraseFromParent();
+    Add->eraseFromParent();
   }
+}
 
-  return changed;
-}
-}
+} // namespace
 
 static RegisterPass<X86KostinPass> X("x86-kostin-pass", "X86 Kostin Pass",
-                                         false, false);
+                                     false, false);
