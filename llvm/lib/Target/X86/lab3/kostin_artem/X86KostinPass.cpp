c7ebd5bbf5e2efe49856d50b8dc849f782de3e81
aartyomm <artem-kostin2013.kostin@yandex.ru>
2024-04-25 20:29:27 +0300

    clang format

--- llvm/lib/Target/X86/lab3/kostin_artem/X86KostinPass.cpp
+++ llvm/lib/Target/X86/lab3/kostin_artem/X86KostinPass.cpp
@@ -21,7 +21,7 @@ char X86KostinPass::ID = 0;
 bool X86KostinPass::runOnMachineFunction(MachineFunction &pFunction) {
   const TargetInstrInfo *instrInfo = pFunction.getSubtarget().getInstrInfo();
   bool isModified = false;
-  std::vector<std::pair<MachineInstr*, MachineInstr*>> toDelete;
+  std::vector<std::pair<MachineInstr *, MachineInstr *>> toDelete;
 
   for (auto &basicBlock : pFunction) {
     MachineInstr *mulInstruction = nullptr;
@@ -31,15 +31,18 @@ bool X86KostinPass::runOnMachineFunction(MachineFunction &pFunction) {
       if (instr.getOpcode() == X86::MULPDrr) {
         mulInstruction = &instr;
 
-        for (auto next = std::next(instr.getIterator()); next != basicBlock.end(); ++next) {
+        for (auto next = std::next(instr.getIterator());
+             next != basicBlock.end(); ++next) {
           if (next->getOpcode() == X86::ADDPDrr) {
             addInstruction = &*next;
-            if (mulInstruction->getOperand(0).getReg() == addInstruction->getOperand(1).getReg()) {
+            if (mulInstruction->getOperand(0).getReg() ==
+                addInstruction->getOperand(1).getReg()) {
               toDelete.emplace_back(mulInstruction, addInstruction);
               isModified = true;
               break;
             }
-          } else if (next->definesRegister(mulInstruction->getOperand(0).getReg())) {
+          } else if (next->definesRegister(
+                         mulInstruction->getOperand(0).getReg())) {
             break;
           }
         }
@@ -48,8 +51,9 @@ bool X86KostinPass::runOnMachineFunction(MachineFunction &pFunction) {
   }
 
   for (const auto &[mulInstr, addInstr] : toDelete) {
-    MachineInstrBuilder builder = BuildMI(*mulInstr->getParent(), *mulInstr, mulInstr->getDebugLoc(),
-                                          instrInfo->get(X86::VFMADD213PDZ128r));
+    MachineInstrBuilder builder =
+        BuildMI(*mulInstr->getParent(), *mulInstr, mulInstr->getDebugLoc(),
+                instrInfo->get(X86::VFMADD213PDZ128r));
     builder.addReg(addInstr->getOperand(0).getReg(), RegState::Define);
     builder.addReg(mulInstr->getOperand(1).getReg());
     builder.addReg(mulInstr->getOperand(2).getReg());
@@ -60,7 +64,7 @@ bool X86KostinPass::runOnMachineFunction(MachineFunction &pFunction) {
 
   return isModified;
 }
-}
+} // namespace
 
 static RegisterPass<X86KostinPass> X("x86-kostin-pass", "X86 Kostin Pass",
                                      false, false);
